b05d79280e382b4100311f8f243f73607391122b
Julia Mysyk <juliaa.mysyk@gmail.com>
2018-07-28 18:12:38 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -5,16 +5,12 @@ row = [" "," "," ",]
 # Position 1: Left
 row[0]
 
-# Position 2: Middle 
+# Position 2: Middle
 row[1]
 
-# Position 3: Right 
+# Position 3: Right
 row[2]
 
 # Move X to the postion 2, Middle
 row[1] =  "X"
 row #=> ['' '',''X'','' '']
-
-
-
-
